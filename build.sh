5f4c789563646cbb417edcfef574a5e6bc84e755
Devon Govett <devongovett@gmail.com>
2011-11-27 12:13:02 -0800

    Remove core.js since it is unused

--- build.sh
+++ build.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-if [ ! -d "vendor" ]; then
-  mkdir vendor
-fi
-
-if [ ! -d "vendor/core.js" ]; then
-  git clone https://github.com/JensNockert/core.js.git vendor/core.js
-fi
-
-cd vendor/core.js && git pull && cd ../..
-
 coffee --join lib/alac.js --compile src/*.coffee
